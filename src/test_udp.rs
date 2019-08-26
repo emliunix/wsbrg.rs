8a7e6df196050abf5f2386fb1b5dc684281df3db
Alan Liu <emliunix@hotmail.com>
2019-08-26 19:12:19 +0800

    test_udp.rs fix BytesMut doesn't reserve space

--- src/test_udp.rs
+++ src/test_udp.rs
@@ -18,21 +18,15 @@ fn blank_io_error() -> IOError {
     IOError::new(IOErrorKind::Other, "some error")
 }
 
-fn general_handler<T, E>(res: Result<T, E>) -> Result<(), std::io::Error>
-where
-    E: std::fmt::Debug,
-{
-    match res {
-        Ok(_) => Ok(()),
-        Err(err) => {
-            use std::io::{Error, ErrorKind};
-            Err(Error::new(
-                ErrorKind::Other,
-                format!("Some error: {:?}", err),
-            ))
-        }
-    }
-}
+// fn general_handler<T, E>(res: Result<T, E>) -> Result<(), std::io::Error> where E: std::fmt::Debug {
+//     match res {
+//         Ok(_) => Ok(()),
+//         Err(err) => {
+//             use std::io::{Error, ErrorKind};
+//             Err(Error::new(ErrorKind::Other, format!("Some error: {:?}", err)))
+//         },
+//     }
+// }
 
 fn test_msg() -> impl Future<Item = (), Error = ()> {
     let from_addr = "0.0.0.0:8000"
@@ -55,6 +49,7 @@ fn test_msg() -> impl Future<Item = (), Error = ()> {
                     println!("Sending test msg[{}], instant = {:?}", msg_id, i);
                     let s: String = format!("a test msg, no. {}", msg_id);
                     let mut buf = BytesMut::new();
+                    buf.reserve(s.as_bytes().len());
                     buf.put(s);
                     (buf.freeze(), to_addr)
                 }),
@@ -67,21 +62,25 @@ pub fn test() {
     let addr = "0.0.0.0:8888"
         .parse::<SocketAddr>()
         .expect("invalid socketAddr");
-    let udpSocket = UdpSocket::bind(&addr).expect("Failed to create UdpSocket");
-    let udpStream = UdpFramed::new(udpSocket, BytesCodec::new());
-    let (sink, istream) = udpStream.split();
+    let udp_socket = UdpSocket::bind(&addr).expect("Failed to create UdpSocket");
+    let udp_stream = UdpFramed::new(udp_socket, BytesCodec::new());
+    let (sink, istream) = udp_stream.split();
     println!("Listening UDP MSG @ {}", addr);
     // tokio::run(sink.send_all(s).then(|res| {
     let t1 = sink
-        .send_all(istream.map(|(b, srcAddr)| {
-            let s: String = format!("FROM {:?} ECHO {:?}", srcAddr, b);
+        .send_all(istream.map(|(b, src_addr)| {
+            let msg_fallback = format!("{:?}", b);
+            let msg = std::str::from_utf8(b.as_ref()).unwrap_or(msg_fallback.as_str());
+            let s: String = format!("FROM {:?} ECHO {}", src_addr, msg);
             println!("Recevied DGRAM {}", s);
             let mut buf = BytesMut::new();
+            buf.reserve(s.as_bytes().len());
             buf.put(s);
-            (buf.freeze(), srcAddr)
+            // sends ECHO msg back to where it's from
+            (buf.freeze(), src_addr)
         }))
-        .map_err(|err| eprintln!("UDP processing failed: {:?}", err))
-        .map(|_| ());
+        .map_err(|err| eprintln!("UDP processing failed: {:?}", err));
+    // .map(|_| ());
     let t2 = test_msg().map_err(|_| ());
     tokio::run(t1.join(t2).map(|_| ()).map_err(|_| ()));
 }
